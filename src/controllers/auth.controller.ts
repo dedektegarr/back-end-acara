9b0c0af90df8721bb50e5152348f12673a90f19d
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2025-01-23 19:50:51 +0700

    feat: store user data into database

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import * as Yup from "yup";
+import UserModel from "../models/user.model";
 
 type TRegister = {
   fullName: string;
@@ -33,9 +34,16 @@ export default {
         confirmPassword,
       });
 
+      const result = await UserModel.create({
+        fullName,
+        username,
+        email,
+        password,
+      });
+
       res.status(200).json({
         message: "Success registration!",
-        data: { fullName, username, email },
+        data: result,
       });
     } catch (error) {
       const err = error as Error;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import express from "express";
 import router from "./routes/api";
 import bodyParser from "body-parser";
+import db from "./utils/database";
 
-const app = express();
+const init = async () => {
+  try {
+    const result = await db();
+    console.log(`database status: ${result}`);
 
-const PORT = 3000;
+    const app = express();
+    const PORT = 3000;
 
-app.use(bodyParser.json());
+    app.use(bodyParser.json());
+    app.use("/api", router);
 
-app.use("/api", router);
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+init();
